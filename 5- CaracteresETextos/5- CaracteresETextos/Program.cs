a790daacb10a95257ae3ad8c816c86415c73125e
Murilo <murilo.ps069@gmail.com>
2021-04-16 17:08:06 -0300

    uso de string e char

---
+++ 5- CaracteresETextos/5- CaracteresETextos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5__CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5");
+
+            char primeraletra = 'a';
+            Console.WriteLine(primeraletra);
+
+            //char com numeros procura o digito correspondente ao codigo 65 = A. ascii table
+
+            primeraletra = (char) 65;
+            Console.WriteLine(primeraletra);
+
+            primeraletra = (char)(primeraletra +1);
+            Console.WriteLine(primeraletra);
+
+            string titulo = "essa é a primeira string " + 2021;
+            Console.WriteLine(titulo);
+
+            string comprimentos = @"-milímetro 
+                -centímtro 
+                -metro 
+                -quilometro ";
+            Console.WriteLine(comprimentos);
+
+
+            Console.WriteLine("Fim da execução, tecle enter para fechar");
+            Console.ReadLine();
+        }
+    }
+}
