350bdb615e82f13e5653a338e9249ebf99539cc2
Murilo <murilo.ps069@gmail.com>
2021-04-22 13:56:15 -0300

    utilização de while

---
+++ 6- calculaPoupanca/6- calculaPoupanca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6__calculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6");
+
+            double ValorInvestido = 1000;
+            int x = 1;
+            
+
+            while (x <= 12)
+            {
+                ValorInvestido = ValorInvestido + ValorInvestido * 0.0036;
+                x += 1;
+            }
+
+            Console.WriteLine("Apos 12 meses terá:  " + ValorInvestido);
+            Console.WriteLine("Execução finalizada, tecle enter para continuar");
+            Console.ReadLine();
+        }
+    }
+}
