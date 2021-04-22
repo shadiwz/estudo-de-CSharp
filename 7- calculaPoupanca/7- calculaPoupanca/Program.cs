029fbbef581074e7958db08880d648c37b9d6c36
Murilo <murilo.ps069@gmail.com>
2021-04-22 14:38:13 -0300

    laço de repetição

---
+++ 7- calculaPoupanca/7- calculaPoupanca/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7__calculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11");
+
+            double ValorInvestido = 1000;
+
+            
+            for(int ContadorMes = 1; ContadorMes <= 12; ContadorMes += 1)
+            {
+                ValorInvestido = ValorInvestido + ValorInvestido * 0.0036;
+            }
+
+            Console.WriteLine("Apos 12 meses você terá:  " + ValorInvestido);
+
+            Console.WriteLine("Execução finalizada, tecle enter para continuar");
+            Console.ReadLine();
+        }
+    }
+}
